73d7dc3588ee745fcc5e31f5d8bf47b656f0d98c
hoanguyen81190 <hoanguyen81190@gmail.com>
2017-05-08 21:25:13 +0200

    finished saving excel files

--- excelWorker.js
+++ excelWorker.js
@@ -25,39 +25,26 @@ exports.saveAsExcel = function(fileName, data) {
       firstSheet: 0, activeTab: 1, visibility: 'visible'
     }
   ]
-  var worksheet = workbook.addWorksheet(data);
-  worksheet.columns = [
-      { header: 'Participant', key: 'id', width: 20, outlineLevel: 2 },
-      { header: 'HSI', key: 'hsi', width: 20 },
-      { header: 'Question', key: 'question', width: 20 },
-      { header: 'Trial', key: 'trial', width: 20 },
-      { header: 'Category', key: 'category', width: 20 },
-      { header: 'Event Start Trial Time [ms]', key: 'start_time', width: 20 },
-      { header: 'Event End Trial Time [ms]', key: 'end_time', width: 20 },
-      { header: 'Event Duration [ms]', key: 'duration', width: 20 },
-      { header: 'Fixation Position X [px]', key: 'fixation_x', width: 20 },
-      { header: 'Fixation Position Y [px]', key: 'fixation_y', width: 20 },
-      { header: 'AOI Name', key: 'aoi', width: 20 },
-      { header: 'Image', key: 'image', width: 20 },
-      { header: 'Response Time', key: 'response_time', width: 20 },
-      { header: 'Answer', key: 'answer', width: 20 },
-      { header: 'Correct Answer', key: 'correct_answer', width: 20 }
-  ];
+  var worksheet = workbook.addWorksheet('Participant' + participantId);
+  var row1 = [];
+  row1.push('Participant ID: ');
+  row1.push(participantId);
+  worksheet.addRow(row1);
 
-  var idCol = worksheet.getColumn('id');
-  var hsiCol = worksheet.getColumn('hsi');
-  var questionCol = worksheet.getColumn('question');
-  var trialCol = worksheet.getColumn('trial');
-  var categoryCol = worksheet.getColumn('category');
-  var startTimeCol = worksheet.getColumn('start_time');
-  var endTimeCol = worksheet.getColumn('end_time');
-  var durationCol = worksheet.getColumn('duration');
-  var fixationXCol = worksheet.getColumn('fixation_x');
-  var fixationYCol = worksheet.getColumn('fixation_y');
-  var aoiCol = worksheet.getColumn('aoi');
-  var imageCol = worksheet.getColumn('image');
-  var answerCol = worksheet.getColumn('answer');
-  var correctAnsCol = worksheet.getColumn('correct_answer');
+  var row2 = [];
+  row2.push("HSI order: ");
+  hsiOrder.map((d, i)=>{
+    row2.push(d);
+  });
+  worksheet.addRow(row2);
+
+  worksheet.addRow();
+  worksheet.addRow();
+
+  var columns = ['Participant', 'HSI', 'Question', 'Trial', 'Category', 'Event Start Trial Time [ms]', 'Event End Trial Time [ms]', 'Event Duration [ms]',
+                'Fixation Position X [px]', 'Fixation Position Y [px]', 'AOI Name', 'Image', 'Response Time', 'Answer', 'Correct Answer'];
+
+  worksheet.addRow(columns);
 
   hsiData.map((hsiItem, hsiIndex) => {
     var hsi = hsiItem.hsi;
@@ -67,27 +54,11 @@ exports.saveAsExcel = function(fileName, data) {
       var trials = qItem.trials;
       trials.map((tItem, tIndex) => {
         var keyResponse = tItem.keyResponse;
-        var img = tItem.image;
         var correctAnswer = tItem.correctAnswer;
         var gazePath = tItem.gazePath;
         gazePath.map((gItem, gIndex) => {
-          worksheet.addRow();
-          worksheet.addRow({
-            id: participantId,
-            hsi: hsi,
-            question: questionId,
-            trial: tIndex,
-            category: gItem.category,
-            start_time: gItem.startTime,
-            end_time: gItem.endTime,
-            duration: gItem.duration,
-            fixation_x: gItem.position.X,
-            fixation_y: gItem.position.Y,
-            aoi: gItem.aoi,
-            image: img,
-            response_time: keyResponse.responseTime,
-            answer: keyResponse.key,
-            correct_answer: correctAnswer});
+          worksheet.addRow([gItem.participantId, hsi, questionId, tIndex, gItem.category, gItem.eventStart, gItem.eventEnd, gItem.eventDuration,
+                            gItem.fixationPos.posX, gItem.fixationPos.posY, gItem.aoiName, gItem.image, keyResponse.responseTime, keyResponse.key, correctAnswer]);
         });
       });
     });
--- pages/home/index.js
+++ pages/home/index.js
@@ -8,6 +8,7 @@ import { title, html } from './index.md';
 import store from '../../core/store';
 import history from '../../core/history';
 
+import excelData from '../../ExperimentDataExample.json';
 
 class HomePage extends React.Component {
   constructor(props){
@@ -28,12 +29,36 @@ class HomePage extends React.Component {
     history.push('/Experiment');
   }
 
+  testSaveExcel() {
+    var request = new Request('http://localhost:3000/api', {
+       method: 'POST',
+       headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+       },
+       redirect: 'follow',
+       body: JSON.stringify({
+          request: 'save data',
+          fileName: './public/experiment/test.xlsx',
+          data: JSON.stringify(excelData)
+      })
+      // mode: 'no-cors'
+    });
+      fetch(request).then(function(response) {
+        return response.json();
+      }).then(function(j) {
+        console.log(j);
+      });
+  }
+
   render() {
     return (
       <div >
         <button onClick={()=>this.handleOpenEditingPage()}>Test Editing Page</button>
         <br/>
         <button onClick={()=>this.handleOpenExperimentPage()}>Test Experiment Page</button>
+        <br/>
+        <button onClick={()=>this.testSaveExcel()}>Test Save Excel Data</button>
       </div>);
   }
 
